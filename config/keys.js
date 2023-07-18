aaeaeada4f0c75402294404a9bf58217e980ce53
Posam Yeswant Reddy <posamyesh@gmail.com>
2023-07-18 14:28:32 +0530

    deploy to railway

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,8 @@
 //we write some logic to find out whether we are in prod(production) or dev(local server)
 
-if(process.env.RAILWAY_ENVIRONMENT === 'production'){
+if(process.env.NODE_ENV === 'production'){
     //we are in production environment
+    module.exports = require('./prod');
 }
 else{
     //we are in local server i,e development environment
